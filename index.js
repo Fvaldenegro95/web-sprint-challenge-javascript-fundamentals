c6e10708d60cfcf99adc20c397892b82b02de7cd
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-09-17 16:18:53 -0400

    index complete readme next

--- index.js
+++ index.js
@@ -215,13 +215,21 @@ class CuboidMakerTwo{
   surfaceArea(){
     return 2 *(this.length * this.width + this.length * this.height + this.width * this.height)
   }
+  volume(){
+    return this.length * this.width * this.height;
+  }
 }
+let cuboidTwo = new CuboidMakerTwo({
+  length: 4,
+  width: 5,
+  height: 5,
+});
 
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
